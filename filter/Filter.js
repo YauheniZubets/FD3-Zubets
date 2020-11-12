13d21dd87c1b157b317f11caede8860ad024da9a
Yauheni Zubets <gunner17djg@gmail.com>
2020-11-12 22:44:29 +0300

    done filter and ishop2

--- filter/Filter.js
+++ filter/Filter.js
@@ -8,18 +8,28 @@ var Filter=React.createClass({
 
     getInitialState: function(){
         return {
-            viewArr: null,
-            sorted: false
+            viewArr: this.props.arrStr,
+            sorted: false,
+            inpValue: '',
+            unsortedArr: null
         }
     },
 
-    sortData: function() {
-        
+    inpChanged: function(EO) {
+        let strToFind=EO.target.value;
+        this.setState({inpValue: strToFind}, this.filterData(strToFind));
     },
 
-    textChanged: function(EO) {
-        let stringToFind=EO.target.value;
-        let copyArr=this.props.arrStr.slice();
+    cbSort: function () {
+        this.setState({sorted: !this.state.sorted}, this.sortData);
+    },
+
+    filterData: function(stringToFind) {
+        let copyArr;
+        copyArr=this.props.arrStr.slice();
+        if (this.state.sorted){
+            copyArr.sort();
+        }
         let filtered=copyArr.filter(item=>{
             if (item.indexOf(stringToFind) != -1){
                 return item
@@ -29,50 +39,31 @@ var Filter=React.createClass({
     },
 
     sortData: function () {
-        let unsortedArr=this.state.viewArr.slice();
-        if(!this.state.sorted){
-            if(this.state.viewArr.length>0){
-                let copySortedArr=this.state.viewArr.slice().sort();
-                this.setState({viewArr: copySortedArr, sorted: true});
-            } else {
-                let copySortedArr=this.props.arrStr.slice().sort();
-                this.setState({viewArr: copySortedArr, sorted: true});
-            }
+        let copyArr=this.state.viewArr.slice();
+        
+        if (this.state.sorted){
+            let unsortedArr=this.state.viewArr.slice();
+            this.setState({viewArr: copyArr.sort(), unsortedArr: unsortedArr});
         } else {
-            if (this.state.viewArr.length>0){
-                console.log(unsortedArr);
-                this.setState({viewArr: unsortedArr, sorted: false});
-            } else {
-                this.setState({viewArr: null, sorted: false})
-            }
+            this.setState({viewArr: this.state.unsortedArr});
         }
     },
 
-    filterData: function () {
-        
-    },
-
-    cbClicked: function () {
-        
+    cbDefault: function () {
+        this.setState({inpValue: '', viewArr: this.props.arrStr, sorted: false});
     },
 
     render: function(){
         
-        if(this.state.viewArr){
-            var resultLi=this.state.viewArr.map((item, index)=>{
-                return React.DOM.li({key: index}, item);
-            });
-        } else {
-            var resultLi=this.props.arrStr.map((item, index)=>{
-                return React.DOM.li({key: index}, item);
-            });
-        }
+        var resultLi=this.state.viewArr.map((item, index)=>{
+            return React.DOM.li({key: index}, item);
+        });
         
         return React.DOM.div({},
             React.DOM.div({className: 'Maininput'}, 
-                React.DOM.input({type: 'checkbox', onChange: this.sortData}),
-                React.DOM.input({type: 'text', onChange: this.textChanged}),
-                React.DOM.input({type: 'button', value: 'сброс', onClick: this.cbClicked}),
+                React.DOM.input({type: 'checkbox', onChange: this.cbSort, checked: this.state.sorted}),
+                React.DOM.input({type: 'text', onChange: this.inpChanged, value: this.state.inpValue}),
+                React.DOM.input({type: 'button', value: 'сброс', onClick: this.cbDefault}),
             ),
             React.DOM.div({className: 'Mainlist'},
                 React.DOM.ul({},
