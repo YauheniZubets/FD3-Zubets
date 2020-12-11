92325210c22ef0a062658dd2211fa811ef8c6997
Yauheni Zubets <gunner17djg@gmail.com>
2020-12-11 23:34:50 +0300

    edit callback DoubleButton

--- RainbowFrameHOC/components/DoubleButton.js
+++ RainbowFrameHOC/components/DoubleButton.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from 'react';
 
 class DoubleButton extends React.Component {
+
+    cbPressed = (num) => {
+        alert(num.target.value);
+    }
     
     render () {
         return (
             <Fragment>
-                <input type='button' onClick={this.props.cbPressed} value={this.props.caption1} />
+                <input type='button' onClick={this.cbPressed} value={this.props.caption1} />
                  {this.props.children} 
-                <input type='button' onClick={this.props.cbPressed} value={this.props.caption2}/>
+                <input type='button' onClick={this.cbPressed} value={this.props.caption2}/>
             </Fragment>
-            
         )
-        
     }
 };
 
